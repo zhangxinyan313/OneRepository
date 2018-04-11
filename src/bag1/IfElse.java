aa702db3299d08ba0bd8611bc0a7d8a2fcd9e31f
zhangxinyan313 <xinyanzhang387@163.com>
2018-04-11 10:11:40 +0800

    this is two commit

---
+++ src/bag1/IfElse.java
@@ -0,0 +1,13 @@
+package bag1;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int number=5;
+		if (number>5) {
+			System.out.println("hello");
+		} else {
+System.out.println("thank you");
+		}
+	}
+
+}
